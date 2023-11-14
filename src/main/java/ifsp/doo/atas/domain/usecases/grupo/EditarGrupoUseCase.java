b0928b2e90f57e04b8224ea40b90bd6a662d706b
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-11-14 19:21:08 -0300

    feat: Implemented editarGrupo in EditarGrupoUseCase

--- src/main/java/ifsp/doo/atas/domain/usecases/grupo/EditarGrupoUseCase.java
+++ src/main/java/ifsp/doo/atas/domain/usecases/grupo/EditarGrupoUseCase.java
@@ -1,4 +1,15 @@
 package ifsp.doo.atas.domain.usecases.grupo;
+
+import ifsp.doo.atas.domain.model.DadosAtualizacaoGrupo;
+import ifsp.doo.atas.domain.model.Grupo;
+import ifsp.doo.atas.domain.model.GrupoRepository;
+import jakarta.validation.Valid;
+
 public class EditarGrupoUseCase {
-    
+    private GrupoRepository repository;
+
+    public void editarGrupo(@Valid DadosAtualizacaoGrupo dados){
+        Grupo grupo = repository.getReferenceById(dados.id());
+        grupo.atualizarGrupo(dados);
+    }
 }
