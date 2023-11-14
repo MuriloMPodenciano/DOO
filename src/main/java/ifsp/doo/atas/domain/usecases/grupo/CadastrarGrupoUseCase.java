5d36de157583797905512befd5044c2a7bc46ea1
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-11-14 19:15:29 -0300

    feat: Implemented CadastrarGrupo in CadastrarGrupoUseCase

--- src/main/java/ifsp/doo/atas/domain/usecases/grupo/CadastrarGrupoUseCase.java
+++ src/main/java/ifsp/doo/atas/domain/usecases/grupo/CadastrarGrupoUseCase.java
@@ -1,4 +1,23 @@
 package ifsp.doo.atas.domain.usecases.grupo;
+
+import ifsp.doo.atas.domain.model.DadosCadastroGrupo;
+import ifsp.doo.atas.domain.model.Grupo;
+import ifsp.doo.atas.domain.model.GrupoRepository;
+import jakarta.validation.Valid;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public class CadastrarGrupoUseCase {
-    
+    private GrupoRepository repository;
+
+    public void cadastrarGrupo(@Valid DadosCadastroGrupo dados){
+        if(repository.existByNome(dados.nome())){
+            throw new DataIntegrityViolationException("Grupo com nome " + dados.nome() + " já existe.");
+        }
+
+        Grupo grupo = new Grupo(dados.nome(), dados.funcionarios());
+        repository.save(grupo);
+
+    }
 }
