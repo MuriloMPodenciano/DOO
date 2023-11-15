63cda73cc875fa3e10f1579a8bbd518ad935f228
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-11-15 03:56:26 -0300

    refactor: Moved DAO things to GrupoDAO
    Now it calls the DAO instead of doing everything by its own.

--- src/main/java/ifsp/doo/atas/domain/usecases/grupo/CadastrarGrupoUseCase.java
+++ src/main/java/ifsp/doo/atas/domain/usecases/grupo/CadastrarGrupoUseCase.java
@@ -3,6 +3,7 @@ package ifsp.doo.atas.domain.usecases.grupo;
 import ifsp.doo.atas.domain.model.DadosCadastroGrupo;
 import ifsp.doo.atas.domain.model.Grupo;
 import ifsp.doo.atas.domain.model.GrupoRepository;
+import ifsp.doo.atas.domain.persistence.GrupoDAO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -12,14 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class CadastrarGrupoUseCase {
     @Autowired
     private GrupoRepository repository;
+    private GrupoDAO dao;
+
+    public CadastrarGrupoUseCase(GrupoRepository repository) {
+        this.repository = repository;
+        this.dao = new GrupoDAO(repository);
+    }
 
     public void cadastrarGrupo(@Valid DadosCadastroGrupo dados){
-        if(repository.existByNome(dados.nome())){
+        if(dao.existsByName(dados.nome())){
             throw new DataIntegrityViolationException("Grupo com nome " + dados.nome() + " já existe.");
         }
-
         Grupo grupo = new Grupo(dados.nome(), dados.funcionarios());
-        repository.save(grupo);
-
+        dao.create(grupo);
     }
 }
