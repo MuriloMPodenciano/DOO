bf38f99f65942a9ed6ac85ea5f4b98cd6213a443
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-11-15 03:52:30 -0300

    refactor: Moved DAO stuff to GrupoDAO
    Now the class will call the DAO instad of doing things by itself.

--- src/main/java/ifsp/doo/atas/domain/usecases/grupo/AlterarStatusGrupoUseCase.java
+++ src/main/java/ifsp/doo/atas/domain/usecases/grupo/AlterarStatusGrupoUseCase.java
@@ -2,6 +2,8 @@ package ifsp.doo.atas.domain.usecases.grupo;
 
 import ifsp.doo.atas.domain.model.Grupo;
 import ifsp.doo.atas.domain.model.GrupoRepository;
+import ifsp.doo.atas.domain.persistence.GrupoDAO;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class AlterarStatusGrupoUseCase {
     @Autowired
     private GrupoRepository repository;
+    private GrupoDAO dao;
 
-    public Grupo alterarStatus(@Valid @NotNull Long id){
-        Grupo grupo = repository.getReferenceById(id);
-        grupo.mudarStatus();
+    public AlterarStatusGrupoUseCase(GrupoRepository repository) {
+        this.dao = new GrupoDAO(repository);
+    }
 
-        return grupo;
+    public String alterarStatus(@Valid @NotNull Long id){
+        if(!dao.get(id).isPresent()){
+            throw new EntityNotFoundException("Grupo " + id + " nao existe.");
+        }
+
+        dao.changeStatus(id);
+        return "Status do grupo alterado com sucesso";
     }
 }
