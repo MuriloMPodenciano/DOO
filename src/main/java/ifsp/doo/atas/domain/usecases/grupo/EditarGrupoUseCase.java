b21b0c208b267ebfad65e98f1bccd9ddab737fac
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-11-15 04:01:09 -0300

    feat: implemented EditarGrupoUseCase

--- src/main/java/ifsp/doo/atas/domain/usecases/grupo/EditarGrupoUseCase.java
+++ src/main/java/ifsp/doo/atas/domain/usecases/grupo/EditarGrupoUseCase.java
@@ -3,6 +3,8 @@ package ifsp.doo.atas.domain.usecases.grupo;
 import ifsp.doo.atas.domain.model.DadosAtualizacaoGrupo;
 import ifsp.doo.atas.domain.model.Grupo;
 import ifsp.doo.atas.domain.model.GrupoRepository;
+import ifsp.doo.atas.domain.persistence.GrupoDAO;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class EditarGrupoUseCase {
     @Autowired
     private GrupoRepository repository;
+    private GrupoDAO dao;
 
-    public void editarGrupo(@Valid DadosAtualizacaoGrupo dados){
-        Grupo grupo = repository.getReferenceById(dados.id());
-        grupo.atualizarGrupo(dados);
+    public EditarGrupoUseCase(GrupoRepository repository) {
+        this.dao = new GrupoDAO(repository);
+    }
+
+    public String editarGrupo(@Valid DadosAtualizacaoGrupo dados){
+        if(!dao.get(dados.id()).isPresent()){
+            throw new EntityNotFoundException("Grupo " + dados.id() + " nao existe.");
+        }
+        Grupo grupo = new Grupo(dados.id(), dados.nome(), dados.funcionarios());
+        dao.update(dados.id(), grupo);
+        return "Grupo editado com sucesso.";
     }
 }
