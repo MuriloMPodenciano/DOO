83386ace8e2002578d5862f4d7945fc3fd399369
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-11-21 19:49:30 -0300

    feat: Added two GetMapping

--- src/main/java/ifsp/doo/atas/domain/controller/GrupoController.java
+++ src/main/java/ifsp/doo/atas/domain/controller/GrupoController.java
@@ -1,12 +1,16 @@
 package ifsp.doo.atas.domain.controller;
 
+import ifsp.doo.atas.domain.DTO.grupo.GrupoGetPersistDTO;
 import ifsp.doo.atas.domain.DTO.grupo.GrupoPutRequestDTO;
 import ifsp.doo.atas.domain.DTO.grupo.GrupoPostRequestDTO;
 import ifsp.doo.atas.domain.DTO.grupo.GrupoGetResponseDTO;
+import ifsp.doo.atas.domain.usecases.grupo.BuscarGrupoUseCase;
 import ifsp.doo.atas.domain.usecases.grupo.CadastrarGrupoUseCase;
 import ifsp.doo.atas.domain.usecases.grupo.EditarGrupoUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/grupo")
@@ -17,6 +21,9 @@ public class GrupoController {
     @Autowired
     private EditarGrupoUseCase editarGrupo;
 
+    @Autowired
+    private BuscarGrupoUseCase buscarGrupo;
+
     @PostMapping
     public GrupoGetResponseDTO cadastrar(@RequestBody GrupoPostRequestDTO dados) {
         return cadastrarGrupo.cadastrarGrupo(dados);
@@ -25,6 +32,16 @@ public class GrupoController {
     @PutMapping("/{id}")
     public GrupoGetResponseDTO atualizar(@RequestBody GrupoPutRequestDTO dados) {
         return editarGrupo.editarGrupo(dados);
+    }
+
+    @GetMapping
+    public List<GrupoGetPersistDTO> getAll(){
+        return buscarGrupo.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GrupoGetPersistDTO getById(@PathVariable Long id){
+        return buscarGrupo.getById(id);
     }
 }
 
