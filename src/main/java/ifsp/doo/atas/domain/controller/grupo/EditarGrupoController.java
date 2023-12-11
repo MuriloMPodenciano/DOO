0390e5f5b86f15dd2f01606e811b6256454330d5
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-12-11 16:51:56 -0300

    feat: Created EditarGrupoController

---
+++ src/main/java/ifsp/doo/atas/domain/controller/grupo/EditarGrupoController.java
@@ -0,0 +1,54 @@
+package ifsp.doo.atas.domain.controller.grupo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class EditarGrupoController {
+    @FXML
+    private Button ButtonAdicionarPessoa;
+
+    @FXML
+    private Button ButtonEditar;
+
+    @FXML
+    private Button ButtonRemoverPessoa;
+
+    @FXML
+    private Button ButtonVoltar;
+
+    @FXML
+    private CheckBox CheckboxAtivo;
+
+    @FXML
+    private ListView<?> ListViewMembros;
+
+    @FXML
+    private ListView<?> ListViewPessoas;
+
+    @FXML
+    private TextField TextFieldNome;
+
+    @FXML
+    void adicionar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remover(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+
+    }
+}
