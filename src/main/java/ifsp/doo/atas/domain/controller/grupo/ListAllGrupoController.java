588c89d23b0bf48ebf3168a99c021371c2d77fa6
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-12-11 16:57:54 -0300

    feat: Created ListAllGrupoController

---
+++ src/main/java/ifsp/doo/atas/domain/controller/grupo/ListAllGrupoController.java
@@ -0,0 +1,31 @@
+package ifsp.doo.atas.domain.controller.grupo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class ListAllGrupoController {
+    @FXML
+    private Button ButtonDetalhes;
+
+    @FXML
+    private Button ButtonVoltar;
+
+    @FXML
+    private Label LabelNomeGrupo;
+
+    @FXML
+    private ListView<?> ListViewGrupos;
+
+    @FXML
+    void detalhes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+
+    }
+}
