e27dd7658c1c8e3c6d04e03706f3e29fc7ee1a27
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-12-13 17:22:45 -0300

    feat: Implemented EditarGrupoController
    Except the functions adicionar and excluir for they might be removed.

--- src/main/java/ifsp/doo/atas/domain/controller/grupo/EditarGrupoController.java
+++ src/main/java/ifsp/doo/atas/domain/controller/grupo/EditarGrupoController.java
@@ -1,12 +1,26 @@
 package ifsp.doo.atas.domain.controller.grupo;
 
+import ifsp.doo.atas.domain.DTO.grupo.GrupoGetPersistDTO;
+import ifsp.doo.atas.domain.DTO.grupo.GrupoPutRequestDTO;
+import ifsp.doo.atas.domain.DTO.pessoa.PessoaGetPersistDTO;
 import ifsp.doo.atas.domain.model.Grupo;
+import ifsp.doo.atas.domain.usecases.grupo.EditarGrupoUseCase;
+import ifsp.doo.atas.domain.usecases.pessoa.BuscarPessoaUseCase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
 
 public class EditarGrupoController {
     @FXML
@@ -25,15 +39,26 @@ public class EditarGrupoController {
     private CheckBox CheckboxAtivo;
 
     @FXML
-    private ListView<?> ListViewMembros;
+    private ListView<PessoaGetPersistDTO> ListViewMembros;
 
     @FXML
-    private ListView<?> ListViewPessoas;
+    private ListView<PessoaGetPersistDTO> ListViewPessoas;
 
     @FXML
     private TextField TextFieldNome;
 
     private Grupo grupo;
+    @Autowired
+    private BuscarPessoaUseCase buscarPessoa;
+    @Autowired
+    private EditarGrupoUseCase editarGrupo;
+
+    @FXML
+    public void initialize(){
+        GrupoGetPersistDTO grupoPersist = new GrupoGetPersistDTO(grupo);
+        ObservableList<PessoaGetPersistDTO> membros = FXCollections.observableList(grupoPersist.funcionarios());
+        ListViewMembros.setItems(membros);
+    }
 
     @FXML
     void adicionar(ActionEvent event) {
@@ -42,7 +67,15 @@ public class EditarGrupoController {
 
     @FXML
     void editar(ActionEvent event) {
+        GrupoGetPersistDTO grupoPersist = new GrupoGetPersistDTO(grupo);
+        GrupoPutRequestDTO grupoModificado = new GrupoPutRequestDTO(grupoPersist.id(), CheckboxAtivo.isSelected());
+        editarGrupo.editarGrupo(grupoModificado);
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ifsp.doo.atas.domain.UI.grupo.DetalhesGrupo"));
+        Scene detalhesGrupo = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(detalhesGrupo);
+        stage.show();
     }
 
     @FXML
@@ -51,8 +84,12 @@ public class EditarGrupoController {
     }
 
     @FXML
-    void voltar(ActionEvent event) {
-
+    void voltar(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ifsp.doo.atas.domain.UI.grupo.DetalhesGrupo"));
+        Scene detalhesGrupo = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(detalhesGrupo);
+        stage.show();
     }
 
     public void setGrupo(Grupo grupo) {
