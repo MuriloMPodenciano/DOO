ed95ca6b731f39695ba463c143c59ee25dada021
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2023-12-13 20:15:45 -0300

    feat: created button methods for PessoaController

--- src/main/java/ifsp/doo/atas/domain/controller/pessoa/ListarPessoaController.java
+++ src/main/java/ifsp/doo/atas/domain/controller/pessoa/ListarPessoaController.java
@@ -1,5 +1,9 @@
 package ifsp.doo.atas.domain.controller.pessoa;
 
+import ifsp.doo.atas.domain.DTO.pessoa.PessoaGetPersistDTO;
+import ifsp.doo.atas.domain.usecases.pessoa.BuscarPessoaUseCase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,9 +11,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.List;
+
 
 public class ListarPessoaController {
     @FXML
@@ -18,6 +27,22 @@ public class ListarPessoaController {
     private Button ButtonVoltar;
     @FXML
     private Button ButtonNovaPessoa;
+    @FXML
+    private ListView<PessoaGetPersistDTO> listViewPessoas;
+
+    private BuscarPessoaUseCase buscarPessoaUseCase;
+
+    @FXML
+    void initialize() {
+        updateListView();
+    }
+    private void updateListView() {
+        List<PessoaGetPersistDTO> pessoas = buscarPessoaUseCase.getAll();
+
+        ObservableList<PessoaGetPersistDTO> observableList = FXCollections.observableArrayList(pessoas);
+
+        listViewPessoas.setItems(observableList);
+    }
 
     @FXML
     void ButtonVoltar(ActionEvent event) throws IOException {
@@ -29,4 +54,32 @@ public class ListarPessoaController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    void ButtonNovaPessoa(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("java/ifsp/doo/atas/interface/local-javafx/pessoa/Criarpessoa.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        dialogStage.initStyle(StageStyle.UNDECORATED);
+        dialogStage.setScene(new Scene(root));
+
+        dialogStage.showAndWait();
+    }
+
+    @FXML
+    void ButtonDetalhesPessoa(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("java/ifsp/doo/atas/interface/local-javafx/pessoa/DetalhesPessoa.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        dialogStage.initStyle(StageStyle.UNDECORATED);
+        dialogStage.setScene(new Scene(root));
+
+        dialogStage.showAndWait();
+
+    }
+
 }
