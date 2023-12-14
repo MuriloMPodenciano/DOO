adf3cd87e738ca24ba7a04fea507bba94510bbf7
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2023-12-13 23:29:57 -0300

    feat: created menu controller buttons and fixed controller link

--- src/main/java/ifsp/doo/atas/UI/menu/MenuController.java
+++ src/main/java/ifsp/doo/atas/UI/menu/MenuController.java
@@ -1,5 +1,37 @@
 package ifsp.doo.atas.UI.menu;
 
-public class MenuController {
+import ifsp.doo.atas.UI.utils.ControllerUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class MenuController {
+    @FXML
+    private Button ButtonExitUsuario;
+    @FXML
+    private Button ButtonListPessoa;
+    @FXML
+    private Button ButtonListGrupo;
+    @FXML
+    private Button ButtonListAta;
+
+    @FXML
+    private void ButtonExitUsuario(ActionEvent event){
+        ControllerUtil.changeScene(event, "java/ifsp/doo/atas/UI/ata/redator.fxml");
+    }
+
+    @FXML
+    private void ButtonListPessoa(ActionEvent event){
+        ControllerUtil.changeScene(event, "java/ifsp/doo/atas/UI/pessoa/ListarPessoa.fxml");
+    }
+
+    @FXML
+    private void ButtonListGrupo(ActionEvent event){
+        ControllerUtil.changeScene(event, "java/ifsp/doo/atas/UI/grupo/ListAllGrupo.fxml");
+    }
+
+    @FXML
+    private void ButtonListAta(ActionEvent event){
+        ControllerUtil.changeScene(event, "");
+    }
 }
